03f3ef79fa5d67507b65618add0faa06d20d9978
EitaIchinose <dellps.psdell@gmail.com>
2021-05-29 21:30:11 +0900

    ユーザー機能・実装

--- backend/resources/views/header.blade.php
+++ backend/resources/views/header.blade.php
@@ -6,21 +6,59 @@
   class Header extends React.Component {
     constructor() {
       super();
+      let csrf_token = document.head.querySelector('meta[name="csrf-token"]').content;
+      this.state = {
+          csrf_token: csrf_token
+      }
+    }
+
+    logOut = () => {
+        event.preventDefault();
+        document.getElementById('logout-form').submit();
+      }
+
+    headerTitle() {
+      return (
+        <div className="header-title">Share.Tweet</div>
+      )
+    }
+
+    guest(){
+      return(
+        <div className="dropdown">
+          <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+            <i className="fa fa-user"></i>
+          </button>
+          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <li><a className="dropdown-item" href="{{ route('register') }}">新規登録</a></li>
+            <li><a className="dropdown-item" href="{{ route('login') }}">ログイン</a></li>
+          </ul>
+        </div>
+      )
     }
 
     render() {
       return(
         <div className="header-container">
-          <div className="header-title">Share.Tweet</div>
-          <div className="dropdown">
-            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-              <i className="fa fa-user"></i>
-            </button>
-            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <li><a className="dropdown-item" href="{{ route('register') }}">新規登録</a></li>
-              <li><a className="dropdown-item" href="{{ route('login') }}">ログイン</a></li>
-            </ul>
-          </div>
+          @guest
+            <this.headerTitle />
+            <this.guest />
+          @endguest
+          @auth
+            <this.headerTitle />
+            <div className="dropdown">
+              <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                <i className="fa fa-user"></i>
+              </button>
+              <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <li><a className="dropdown-item" href="#">マイページ</a></li>
+                <li><a className="dropdown-item" href="#" onClick={this.state.logOut} >ログアウト</a></li>
+                <form method="POST" action="{{ route('logout') }}" id="logout-form">
+                  <input type="hidden" name="_token" value={ this.state.csrf_token } /> 
+                </form>
+              </ul>
+            </div>
+          @endauth
         </div>
       )
     }
--- backend/resources/views/tweets/index.blade.php
+++ backend/resources/views/tweets/index.blade.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>トップページ</title>
 
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
