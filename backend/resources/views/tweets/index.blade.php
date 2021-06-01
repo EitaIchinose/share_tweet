a776a675c7f5686974661b8356222f35e3bd8eac
EitaIchinose <dellps.psdell@gmail.com>
2021-06-01 12:02:17 +0900

    トップページ雛形作成

--- backend/resources/views/tweets/index.blade.php
+++ backend/resources/views/tweets/index.blade.php
@@ -1,41 +1,25 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- CSRF Token -->
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>トップページ</title>
+@extends('layout')
 
-  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/header.css') }}">
+@section('content')
+<div id="main_root"></div>
 
-  <!-- Bootstrap5.0 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<script type="text/babel">
+  const MAIN = () => {
+    return (
+      <div className="main_container">
+        <div className="tweet_content">ツイート内容</div>
+        <div className="img_content">画像</div>
+        <div className="user_container">
+          <div className="good_content">いいね</div>
+          <div className="user_content">by ユーザー名</div>
+        </div>
+      </div>
+    )
+  }
 
-  <!-- Font Awesome -->
-  <link href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" rel="stylesheet">
-
-  <!-- React -->
-  <script src="https://unpkg.com/react@17/umd/react.development.js"></script>
-  <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
-  <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
-
-</head>
-<body>
-  <header>
-    @include('header')
-  </header>
-
-  <main>
-  </main>
-
-  <footer class="footer bg-dark">
-    @include('footer')
-  </footer>
-
-  <!-- Bootstrap.js -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-</body>
-</html>
+  ReactDOM.render(
+    <MAIN />,
+    document.getElementById('main_root')
+  )
+</script>
+@endsection
