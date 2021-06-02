d9f8a9f19687c4549f204da6e9702c091a4ca8be
EitaIchinose <dellps.psdell@gmail.com>
2021-06-02 18:40:32 +0900

    Dropdown修正

--- backend/resources/views/header.blade.php
+++ backend/resources/views/header.blade.php
@@ -23,48 +23,34 @@
       )
     }
 
-    guest(){
-      return(
-        <div className="icon_container">
-          <div className="dropdown">
-            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-              <i className="fa fa-user"></i>
-            </button>
-            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <li><a className="dropdown-item" href="{{ route('register') }}">新規登録</a></li>
-              <li><a className="dropdown-item" href="{{ route('login') }}">ログイン</a></li>
-            </ul>
-          </div>
-        </div>
-      )
-    }
-
     render() {
       return(
         <div className="header-container">
-          @guest
-            <this.headerTitle />
-            <this.guest />
-          @endguest
-          @auth
-            <this.headerTitle />
-            <div className="icon_container">
-              <div className="dropdown">
-                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                  <i className="fa fa-user"></i>
-                </button>
-                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <li><a className="dropdown-item" href="#">マイページ</a></li>
-                  <li>
-                    <a className="dropdown-item" href='#' onClick={this.logOut} >ログアウト</a>
-                    <form method="POST" action="{{ route('logout') }}" id="logout-form">
-                      <input type="hidden" name="_token" value={ this.state.csrf_token } /> 
-                    </form>
-                  </li>
-                </ul>
-              </div>
+          <this.headerTitle />
+          <div className="icon_container">
+            <div className="dropdown">
+              <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                <i className="fa fa-user"></i>
+              </button>
+              @guest
+              <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <li><a className="dropdown-item" href="{{ route('register') }}">新規登録</a></li>
+                <li><a className="dropdown-item" href="{{ route('login') }}">ログイン</a></li>
+              </ul>
+              @endguest
+              @auth
+              <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <li><a className="dropdown-item" href="#">マイページ</a></li>
+                <li>
+                  <a className="dropdown-item" href='#' onClick={ this.logOut } >ログアウト</a>
+                  <form method="POST" action="{{ route('logout') }}" id="logout-form">
+                    <input type="hidden" name="_token" value={ this.state.csrf_token } /> 
+                  </form>
+                </li>
+              </ul>
+              @endauth
             </div>
-          @endauth
+          </div>
         </div>
       )
     }
